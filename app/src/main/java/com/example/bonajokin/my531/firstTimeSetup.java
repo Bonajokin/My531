e58dbbc73b1134b0b0864b34c930dd9abc7045d7
Bonajokin <Bonafide.aa@gmail.com>
2018-07-28 20:06:58 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/bonajokin/my531/firstTimeSetup.java
@@ -0,0 +1,15 @@
+package com.example.bonajokin.my531;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class firstTimeSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_setup);
+
+    }
+}
